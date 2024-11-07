bede5c90df0ddf5adad2910a15ae0b0be87b975b
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-11-06 22:33:37 -0300

    deleted files csharp

--- microservice/src/API/DTOs/UserLoginDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace UserAuth.API.DTOs
-{
-    public class UserLoginDTO
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}

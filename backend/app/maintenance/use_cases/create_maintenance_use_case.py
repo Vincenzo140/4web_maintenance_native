ef48c807d22b96b3866da33b656ab6fd6f21cc0f
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-11-21 17:30:12 -0300

    feat: put some new files on project

--- backend/app/maintenance/use_cases/create_maintenance_use_case.py
+++ backend/app/maintenance/use_cases/create_maintenance_use_case.py
@@ -1,13 +1,13 @@
 from app.base_use_case import BaseUseCase
 from app.maintenance.schemas import CreateMaintenanceSchema 
 from infrastructure.logger import Logger
-from services.machines_service import MachineService
+from services.maintenance_service import MaintenanceService
 
 
 class CreateMachineUseCase(BaseUseCase):
-    def __init__(self, machine_service: MachineService, logger: Logger):
-        self._form_service = machine_service
+    def __init__(self, machine_service: MaintenanceService, logger: Logger):
+        self._machine_service = machine_service
         self._logger = logger
 
-    async def run(self, machine_data: CreateMachinesSchema):
-        await self._form_service.create(machine_data)
+    async def run(self, machine_data: CreateMaintenanceSchema):
+        await self._machine_service.create(machine_data)

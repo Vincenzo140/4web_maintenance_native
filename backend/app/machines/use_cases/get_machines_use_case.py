1ffa8f39770fbd1dd66e0f4b9638e01afd925d04
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-11-21 16:39:30 -0300

    feat: implement get_all machine use case on machine

--- backend/app/machines/use_cases/get_machines_use_case.py
+++ backend/app/machines/use_cases/get_machines_use_case.py
@@ -1,13 +1,12 @@
+from bson import ObjectId
 from app.base_use_case import BaseUseCase
 from infrastructure.logger import Logger
 from services.machines_service import MachineService
 
-
-class GetAllMachineUseCase(BaseUseCase):
-    def __init__(self,  machine_service: MachineService, logger: Logger):
+class GetMachineUseCase(BaseUseCase):
+    def __init__(self, machine_service: MachineService, logger: Logger):
         self._machine_service = machine_service
         self._logger = logger
 
-    async def run(self, start_date: str, end_date: str, limit: int, offset: int):
-        return await self._machine_service.get_all(start_date=start_date, end_date=end_date,
-                                        limit=limit, offset=offset)
+    async def run(self, machine_id: str):
+        return await self._machine_service.get_by_id(machine_id)

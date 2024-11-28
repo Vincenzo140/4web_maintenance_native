fd96e6908c58c5f80796a9d88de79f8274343b91
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-11-28 13:05:57 -0300

    feat: implement redis config on config.py

--- backend/app/users/controller.py
+++ backend/app/users/controller.py
@@ -41,13 +41,13 @@ from fastapi import (
     APIRouter,
     FastAPI
 )
-
+from config import Config
 
 router = APIRouter()
 
 logger = AppLogger().get_logger()
 
-redis_client = redis.ConnectionPool(host='localhost', port=6379, db=0)
+redis_client = redis.ConnectionPool(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_DB)
 
 
 # Função para obter o usuário
--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,10 @@
 class Config:
+    # Api configuration
     port=8000
+    host= 'localhost'
     
+    
+    # Redis configuration
+    REDIS_HOST = 'redis'
+    REDIS_PORT = 6379
+    REDIS_DB = 0
